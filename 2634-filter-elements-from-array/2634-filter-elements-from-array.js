692ee8b8713b7a631bf26e1cf73e0ce9b3c8744e
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-08-21 07:49:45 +0900

    Time: 42 ms (97.56%), Space: 41.8 MB (56.83%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -7,10 +7,8 @@ var filter = function(arr, fn) {
     const result = [];
     
     for (let i = 0; i < arr.length; i++) {
-        console.log(fn(arr[i], i))
         if (fn(arr[i], i)) {
             result.push(arr[i]);
-            
         }
     }
     
