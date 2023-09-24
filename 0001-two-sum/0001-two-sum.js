db0282887560c578d39535564d1438a7ccd7cbec
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-09-25 08:00:53 +0900

    Time: 87 ms (37.36%), Space: 42.2 MB (70.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const result = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (target === nums[i] + nums[j]) {
+                result.push(i);
+                result.push(j);
+                
+                return result;
+            }
+            
+        }
+    }    
+};
