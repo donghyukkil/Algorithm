189d4f6ee6f7d015614a83e0ec71be74d405f8d0
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-10-17 06:31:42 +0900

    Time: 48 ms (77.10%), Space: 42 MB (69.91%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -6,9 +6,9 @@ var maximumWealth = function(accounts) {
     let result = 0;
     
     for (let i = 0; i < accounts.length; i++) {
-        if (result < accounts[i].reduce((acc, cur) => acc + cur, 0)) {
-            result = accounts[i].reduce((acc, cur) => acc + cur, 0);
-        }
+        let temp = accounts[i].reduce((acc, cur) => acc + cur, 0);
+        
+        result = Math.max(result, temp);
     }
     
     return result;
