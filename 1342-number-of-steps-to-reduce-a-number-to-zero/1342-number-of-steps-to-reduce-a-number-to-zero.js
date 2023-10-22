3458400427ad6331d3e0a5c9ce1bd6a837c8669f
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-10-22 09:05:54 +0900

    Time: 46 ms (84.12%), Space: 42 MB (39.94%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let step = 0;
+    
+    return isEvenNumber(num);
+    
+    function isEvenNumber (number) {
+        if (number === 0) {
+            return step;
+        }
+        
+        if (number % 2 === 0) {
+            step++;
+            return isEvenNumber(number / 2);
+        } else {
+            step++;
+            return isEvenNumber(number - 1);
+        }
+    }
+};
