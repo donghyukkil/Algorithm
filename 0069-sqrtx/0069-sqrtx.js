fac189f7a7aaaf13793a400ac7ece3964dc9131d
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-10-25 07:48:35 +0900

    Time: 65 ms (52.89%), Space: 43.8 MB (49.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,5 +3,26 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x));
-};
+    // 1차 제출
+    // return Math.floor(Math.sqrt(x));
+
+    // binarySearch
+    
+    let left = 1;
+    let right = Math.floor(x / 2) + 1;
+    let mid;
+    
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        
+        if (mid * mid > x) {
+            right = mid - 1;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    
+    return right;
+}; 
