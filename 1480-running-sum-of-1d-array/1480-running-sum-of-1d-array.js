05c4cb0cf8a46e50d2223250ff4bcb5b45663719
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-10-26 17:47:54 +0900

    Time: 52 ms (63.04%), Space: 42.7 MB (16.89%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,17 +2,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    const result = [];
-    let calculateNums = 0;
+// var runningSum = function(nums) {
+//     const result = [];
+//     let calculateNums = 0;
     
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j <= i; j++) {
-            calculateNums += nums[j];
-        }
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = 0; j <= i; j++) {
+//             calculateNums += nums[j];
+//         }
         
-         result.push(calculateNums);
-         calculateNums = 0;
+//         result.push(calculateNums);
+//         calculateNums = 0;
+//     }
+    
+//     return result;
+// };
+
+var runningSum = function(nums) {
+    const result = [nums[0]];
+    
+    for (let i = 1; i < nums.length; i++) {
+        result[i] = result[i - 1] + nums[i];
     }
     
     return result;
