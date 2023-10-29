d6296a3ec28a3a73becf5a5694fcbbd4efcad30c
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-10-30 06:51:09 +0900

    Time: 56 ms (25.86%), Space: 41.8 MB (67.03%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.js
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -2,12 +2,28 @@
  * @param {string} s
  * @return {string}
  */
+// var toLowerCase = function(s) {
+//     let str = "";
+    
+//     for (let i = 0; i < s.length; i++) {
+//         str += s[i].toLowerCase();
+//     }
+    
+//     return str;
+// };
+
 var toLowerCase = function(s) {
-    let str = "";
+    let string = "";
     
     for (let i = 0; i < s.length; i++) {
-        str += s[i].toLowerCase();
+        let charCode = s.charCodeAt(i);
+        
+        if (charCode < 91 && charCode > 64) {
+            string += String.fromCharCode(charCode + 32);
+        } else {
+            string += s.charAt(i);
+        }
     }
     
-    return str;
-};
+    return string;
+}
