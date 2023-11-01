761d51140ea5288a5051fea1d09960586c436bb3
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-02 06:00:04 +0900

    Time: 56 ms (50.08%), Space: 42.4 MB (57.47%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let result = 0
+    
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i] === "X++" || operations[i] === "++X") {
+            result++;
+            
+            continue;
+        }
+        
+         if (operations[i] === "X--" || operations[i] === "--X") {
+            result--
+            
+            continue;
+        }
+    }
+    
+    return result;
+};
