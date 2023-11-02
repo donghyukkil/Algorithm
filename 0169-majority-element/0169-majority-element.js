20d1387a7edd2ed89486d5ba8a4e7ef46f3ed544
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-03 06:54:00 +0900

    Time: 2972 ms (5.01%), Space: 43 MB (91.39%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const obj = {};
+    let count = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+            
+            continue;
+        }
+        
+        obj[nums[i]] = count;
+        count = 0;
+    }
+    
+    let maxCount = 0;
+    let majorityElement = nums[0];
+    
+     for (let num in obj) {
+        if (obj[num] > maxCount) {
+            maxCount = obj[num];
+            majorityElement = parseInt(num);
+        }
+    }
+    
+    return majorityElement;
+};
