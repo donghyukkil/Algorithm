3517ded3de6dd88e1180748481074f58121c242f
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-05 11:41:47 +0900

    Time: 66 ms (96.95%), Space: 49.1 MB (26.93%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -1,32 +1,51 @@
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[][]}
+//  */
+// var findDifference = function(nums1, nums2) {
+//     const result1 = [];
+//     const result2 = [];
+    
+//     for (let i = 0; i < nums1.length; i++) {
+//         if (nums2.includes(nums1[i])) {
+//             continue;
+//         }
+        
+//         if (!result1.includes(nums1[i])) {
+//             result1.push(nums1[i]);
+//         }
+        
+//     }
+    
+//     for (let i = 0; i < nums2.length; i++) {
+//         if (nums1.includes(nums2[i])) {
+//             continue;
+//         }
+        
+//         if (!result2.includes(nums2[i])) {
+//             result2.push(nums2[i]);
+//         }
+//     }
+    
+//     return [result1, result2];
+// };
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[][]}
  */
-var findDifference = function(nums1, nums2) {
-    const result1 = [];
-    const result2 = [];
+var findDifference = function(nums1, nums2) {   
+    const answer1 = new Set(nums1);
+    const answer2 = new Set(nums2);
     
-    for (let i = 0; i < nums1.length; i++) {
-        if (nums2.includes(nums1[i])) {
-            continue;
-        }
-        
-        if (!result1.includes(nums1[i])) {
-            result1.push(nums1[i]);
-        }
-        
-    }
-    
-    for (let i = 0; i < nums2.length; i++) {
-        if (nums1.includes(nums2[i])) {
-            continue;
-        }
-        
-        if (!result2.includes(nums2[i])) {
-            result2.push(nums2[i]);
+    for (let item of answer1) {
+        if (answer2.has(item)) {
+            answer1.delete(item);
+            answer2.delete(item);
         }
     }
     
-    return [result1, result2];
+    return [Array.from(answer1), Array.from(answer2)];
 };
