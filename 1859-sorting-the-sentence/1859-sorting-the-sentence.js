e062c4b8dcbc8d3b248dea64da64db033ec3872f
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-11 06:46:12 +0900

    Time: 43 ms (89.85%), Space: 41.9 MB (38.92%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function(s) {
+    const arr = s.split(" ");
+    let result = [];
+    
+    arr.sort((a,b) => Number(a[a.length - 1]) - b[b.length - 1]);
+    
+    for (let i = 0; i < arr.length; i++) {
+        let element = "";
+        
+        for (let j = 0; j < arr[i].length - 1; j++) {
+            element += arr[i][j];
+        }
+
+        result.push(element);
+    }
+    
+    return result.join(" ");
+};
+
