783976911330183894795560357f464ee78462e9
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-15 06:48:38 +0900

    Time: 50 ms (76.36%), Space: 41.5 MB (73.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,10 +2,24 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    if (n <= 1) {
-        return n;
-    } 
+// var fib = function(n) {
+//     if (n <= 1) {
+//         return n;
+//     } 
     
-    return fib(n - 1) + fib(n - 2);
+//     return fib(n - 1) + fib(n - 2);
+// };
+
+const cache = {0: 0, 1: 1};
+
+const fib = (n) => {
+    if (n === 0 || n === 1) {
+      return cache[n];
+    }
+    
+    if (!cache[n]) {
+      cache[n] = fib(n - 1) + fib(n - 2);
+    }
+
+    return cache[n];
 };
