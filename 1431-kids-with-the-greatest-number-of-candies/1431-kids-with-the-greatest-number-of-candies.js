6d50d3b68beac97b36047b31427e57224eff0f5b
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-17 06:04:48 +0900

    Time: 53 ms (62.74%), Space: 42.7 MB (25.76%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const result = [];
+    const maxValue = Math.max(...candies);
+    
+    for (let i = 0; i < candies.length; i++) {
+        candies[i] + extraCandies >= maxValue ? result.push(true) : result.push(false);           
+    }
+    
+    return result;
+};
