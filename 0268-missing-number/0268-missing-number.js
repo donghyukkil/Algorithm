96ec1d4be3714e04f39fb2ad343178be49c19b1f
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-18 06:01:21 +0900

    Time: 448 ms (5.27%), Space: 44.5 MB (37.30%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let answer;
+    
+    for (let i = 0; i <= nums.length; i++) {
+        if (!nums.includes(i)) {
+            answer = i;
+        }
+    }
+    
+    return answer;
+};
