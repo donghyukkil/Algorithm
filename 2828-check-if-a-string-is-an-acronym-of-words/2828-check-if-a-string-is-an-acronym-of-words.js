27798491641b52ca4711736b2a6666fd32a3faa5
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-26 19:02:14 +0900

    Time: 75 ms (33.56%), Space: 45.3 MB (10.83%) - LeetHub

---
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function(words, s) {
+    let str = "";
+    
+    for (let i = 0; i < words.length; i++) {
+        str += words[i][0];
+    }
+    
+    return str === s ? true : false;
+};
