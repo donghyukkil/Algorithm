aa763b163a97c19590bc2884812571b7d8ab190a
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-11-27 19:14:11 +0900

    Time: 59 ms (84.96%), Space: 45.6 MB (25.79%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRightDifference = function(nums) {
+    if (nums.length === 1) {
+        return [0];
+    }
+    
+    const leftSum = [];
+    const rightSum = [];
+    const answer = [];
+    let result = 0;
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        leftSum[0] = 0
+        leftSum[i + 1] = result + nums[i];
+        result += nums[i];
+    }
+    
+    result = 0;
+    
+    for (let i = 1; i < nums.length; i++) {
+        rightSum[nums.length - 1] = 0;
+        rightSum[nums.length - 1 - i] = result + nums[nums.length - i];
+        result += nums[nums.length - i]
+    }
+    
+    for (let i = 0; i < leftSum.length; i++) {
+        answer.push(Math.abs(leftSum[i] - rightSum[i]));
+    }
+    
+    return answer;
+};
