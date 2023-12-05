1f4fd03912fb7dd0f01b8d273d007438b6cf0dd7
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-12-06 06:40:40 +0900

    Time: 51 ms (60.09%), Space: 41.9 MB (57.50%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -1,18 +1,34 @@
-/**
- * @param {number[]} gain
- * @return {number}
- */
-var largestAltitude = function(gain) {
-    let startAltitude = 0;
-    const array = [];
+// /**
+//  * @param {number[]} gain
+//  * @return {number}
+//  */
+// var largestAltitude = function(gain) {
+//     let startAltitude = 0;
+//     const array = [];
     
-    array.push(startAltitude);
+//     array.push(startAltitude);
+    
+//     for (let i = 0; i < gain.length; i++) {
+//         startAltitude += gain[i];
+        
+//         array.push(startAltitude);
+//     }
+    
+//     return Math.max(...array);
+// };
+
+const largestAltitude = function (gain) {
+    let resultAltitude = 0;
+    let highestAltitude = gain[0];
+    let answer;
     
     for (let i = 0; i < gain.length; i++) {
-        startAltitude += gain[i];
+        resultAltitude += gain[i];
         
-        array.push(startAltitude);
+        if (resultAltitude > highestAltitude) {
+             highestAltitude = resultAltitude
+        }
     }
     
-    return Math.max(...array);
+    return highestAltitude > 0 ? highestAltitude : 0;
 };
