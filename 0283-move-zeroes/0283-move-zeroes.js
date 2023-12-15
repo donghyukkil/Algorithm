196da11b235d2512b0a106765e8ee468c12be3c4
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-12-16 06:11:46 +0900

    Time: 77 ms (55.02%), Space: 46.9 MB (27.50%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,16 +2,38 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    let answer = nums;
+
+// selection sort
+// var moveZeroes = function(nums) {
+//     let answer = nums;
     
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === 0 && nums[j] !== 0) {
-                [nums[i], nums[j]] = [nums[j], nums[i]];
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === 0 && nums[j] !== 0) {
+//                 [nums[i], nums[j]] = [nums[j], nums[i]];
+//             }
+//         }
+//     }
+    
+//     return answer;
+// };
+
+// two pointers
+var moveZeroes = function (nums) {
+    let low = 0;
+    let high = low + 1;
+    
+    while (high <= nums.length - 1) {
+        if (nums[low] !== 0) {
+            low++;
+            high++;
+        } else {
+            if (nums[high] !== 0) {
+                [nums[low], nums[high]] = [nums[high], nums[low]];
+                low++;
             }
+            
+            high++;
         }
     }
-    
-    return answer;
 };
