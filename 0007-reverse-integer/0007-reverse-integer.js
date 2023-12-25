ab0886a7b42123e6639e5d5f28d0a085afbc57a1
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-12-25 11:17:59 +0900

    Time: 66 ms (35.10%), Space: 43.8 MB (59.60%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const str = x.toString();
+    let result = "";
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    
+    const reverseNum = parseInt(result, 10);
+    
+    if (reverseNum < Math.pow(-2, 31) || reverseNum > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+    
+    if (x < 0) {
+        return - Number(result.slice(0, result.length - 1));
+    }
+    
+    if (str[str.length - 1] === "0") {        
+        return Number(result.slice(1));
+    }
+    
+    return result;
+};
