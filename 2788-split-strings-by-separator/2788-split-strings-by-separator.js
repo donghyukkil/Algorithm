df136127d25db4d5c2b9a5c4c52c74dc21fa2355
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2023-12-26 15:51:33 +0900

    Time: 92 ms (36.93%), Space: 48.4 MB (64.36%) - LeetHub

---
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} words
+ * @param {character} separator
+ * @return {string[]}
+ */
+var splitWordsBySeparator = function(words, separator) {
+    const result = [];
+    
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(separator)) {
+            words[i].split(separator).forEach((ele) => result.push(ele));
+            continue;
+        }
+        
+        result.push(words[i]);
+    }
+    
+    return result.includes("") ? result.filter(ele => ele !== "") : result;
+};
