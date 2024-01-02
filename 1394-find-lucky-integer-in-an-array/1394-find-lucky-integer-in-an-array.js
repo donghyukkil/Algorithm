b1678a0776c543c98746213fc8ec5ed679a6122f
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2024-01-03 06:56:00 +0900

    Time: 108 ms (5.20%), Space: 44.5 MB (26.24%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -3,36 +3,57 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-//     const map = new Map();
+    const map = new Map();
     
-//     for (const num of arr) {
-//         if (map.has(num)) {
-//             map.set(num, map.get(num) + 1);
-//         } else {
-//             map.set(num, 1);
-//         }
-//     }
-    const obj = {};
-    let count = 0;
-    
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i] === arr[j]) {
-                count++;
-            }
-        }
-        
-        obj[arr[i]] = count;
-        count = 0;
+    for (const num of arr) {
+        map.set(num, (map.get(num) || 0) + 1);
     }
     
     const result = [];
     
-    for (const key in obj) {
-        if (Number(obj[key]) === Number(key)) {
-            result.push(Number(key));
+    for (const [num, count] of map) {
+        if (num === count) {
+            result.push(num);
         }
     }
     
     return result.length > 0 ? Math.max(...result) : - 1;
+//     const obj = {};
+//     let count = 0;
+    
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length; j++) {
+//             if (arr[i] === arr[j]) {
+//                 count++;
+//             }
+//         }
+        
+//         obj[arr[i]] = count;
+//         count = 0;
+//     }
+    
+//     const result = [];
+    
+//     for (const key in obj) {
+//         if (Number(obj[key]) === Number(key)) {
+//             result.push(Number(key));
+//         }
+//     }
+    
+//     return result.length > 0 ? Math.max(...result) : - 1;
+//     const obj = {};
+
+//     for (let i = 0; i < arr.length; i++) {
+//         obj[arr[i]] = (obj[arr[i]] || 0) + 1;
+//     }
+
+//     const result = [];
+
+//     for (const key in obj) {
+//         if (obj[key] === Number(key)) {
+//             result.push(Number(key));
+//         }
+//     }
+
+//     return result.length > 0 ? Math.max(...result) : -1;
 };
