479b3c957a34e9d7f5427c556ffd885b8c0ca4b4
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2024-01-05 06:47:46 +0900

    Time: 60 ms (82.24%), Space: 45.6 MB (25.95%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.js
+++ 2032-two-out-of-three/2032-two-out-of-three.js
@@ -5,33 +5,58 @@
  * @return {number[]}
  */
 var twoOutOfThree = function(nums1, nums2, nums3) {
+//     const newSet1 = new Set(nums1);
+//     const newSet2 = new Set(nums2);
+//     const newSet3 = new Set(nums3);
+    
+//     const result = [];
+    
+//     for (const value of newSet1) {
+//        if (newSet2.has(value) || newSet3.has(value)) {
+//            result.push(value);
+//        }
+//     }
+    
+//     for (const value of newSet2) {
+//         if (newSet1.has(value) || newSet3.has(value)) {
+//             if (!result.includes(value)) {
+//                 result.push(value);
+//             }
+//         }
+//     }
+    
+//     for (const value of newSet3) {
+//         if (newSet1.has(value) || newSet2.has(value)) {
+//             if (!result.includes(value)) {
+//                 result.push(value);
+//             }
+//         }
+//     }
+    
+//     return result;
     const newSet1 = new Set(nums1);
     const newSet2 = new Set(nums2);
     const newSet3 = new Set(nums3);
     
-    const result = [];
+    const result = new Set();
+    
+    const checkAndAdd = (value, set1, set2) => {
+        if (set1.has(value) || set2.has(value)) {
+            result.add(value);
+        }
+    };
     
     for (const value of newSet1) {
-       if (newSet2.has(value) || newSet3.has(value)) {
-           result.push(value);
-       }
+        checkAndAdd(value, newSet2, newSet3);
     }
     
     for (const value of newSet2) {
-        if (newSet1.has(value) || newSet3.has(value)) {
-            if (!result.includes(value)) {
-                result.push(value);
-            }
-        }
+        checkAndAdd(value, newSet1, newSet3);
     }
     
     for (const value of newSet3) {
-        if (newSet1.has(value) || newSet2.has(value)) {
-            if (!result.includes(value)) {
-                result.push(value);
-            }
-        }
+        checkAndAdd(value, newSet1, newSet2);
     }
     
-    return result;
+    return Array.from(result);
 };
