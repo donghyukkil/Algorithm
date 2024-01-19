59445a6f6d264f427b258422bb4bba7ec6881c23
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2024-01-20 06:31:09 +0900

    Time: 159 ms (5.00%), Space: 52.4 MB (5.58%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -3,8 +3,6 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    const answer = nums;
-    
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = 0; j < nums.length - 1 - i; j++) {
             if (nums[j] % 2 !== 0 && nums[j + 1] % 2 === 0) {
@@ -13,5 +11,5 @@ var sortArrayByParity = function(nums) {
         }
     }
     
-    return answer;
+    return nums;
 };
