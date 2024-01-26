5b801cec2770fd442fbff0e3df87ac3d87b002d8
donghyukkil <124029691+donghyukkil@users.noreply.github.com>
2024-01-26 09:30:46 +0900

    Time: 54 ms (69.64%), Space: 52.4 MB (5.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -3,20 +3,39 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {    
-    let result;
-    let isTrue = false;
+// var search = function(nums, target) {    
+//     let result;
+//     let isTrue = false;
     
-    nums.forEach((ele, i) => {
-        if (ele === target) {
-            result = i;
-            isTrue = !isTrue;
+//     nums.forEach((ele, i) => {
+//         if (ele === target) {
+//             result = i;
+//             isTrue = !isTrue;
+//         }
+//     });
+    
+//     if (isTrue) {
+//         return result;
+//     } else {
+//         return - 1; 
+//     }
+// };
+
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-    });
-    
-    if (isTrue) {
-        return result;
-    } else {
-        return - 1; 
     }
+
+    return -1;
 };
